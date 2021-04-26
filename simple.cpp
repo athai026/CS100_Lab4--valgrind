d75772913283b35ee93d5b251bde024917b80dac
alice-thaitea <athai026@ucr.edu>
2021-04-25 23:36:58 -0700

    Fixed delete type error

--- simple.cpp
+++ simple.cpp
@@ -2,6 +2,6 @@ int main() {
     int *p = new int[10];
     p[9] = 1; // Correct this to size - 1 = 10 - 1 = 9
 
-    delete p; // Deallocate the memory from the new above
+    delete[] p; // Deallocate the memory from the new above
     return 0;
 }
