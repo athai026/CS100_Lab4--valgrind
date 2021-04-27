5ddfa89d6952e303c55b79abdcd89d8b0179d107
alice-thaitea <athai026@ucr.edu>
2021-04-27 15:29:40 -0700

    Changed destructor parameter in personList.cpp

--- personList.cpp
+++ personList.cpp
@@ -12,7 +12,10 @@ PersonList::PersonList(){
 }
 
 PersonList::~PersonList(){
-    delete [] theList;
+    for (int i = 0; i < numPeople; ++i) {
+        delete theList[i];
+    }
+    delete[] theList;
 }
 
 void PersonList::addPerson(const char* child_name, const char* father_name, const char* mother_name){
@@ -45,6 +48,7 @@ void PersonList::addPerson(const char* child_name, const char* father_name, cons
     insertIntoList(newChild);
     father->addChild(newChild);
     mother->addChild(newChild);
+
 }
 
 void PersonList::insertIntoList(Person *newPerson){
